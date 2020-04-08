6b25d8904f6de01d38ed6fe8c999286200d8d662
Ben <bbjberg91@gmail.com>
2020-04-08 15:05:40 -0400

    server up/init commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code away!
+
+const express = require('express');
+const server = express();
+const port = 5000;
+server.listen(port, () =>{
+    console.log('server running on port 5k');
+});
+
+server.use(express.json());
